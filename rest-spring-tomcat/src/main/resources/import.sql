3dc75baae84fed663c0d4095639558c6377049a0
Anurag Sharma <anurag.sharma@sixturtle.com>
2016-05-13 13:57:42 -0400

    adding one more record in the import.sql

--- rest-spring-tomcat/src/main/resources/import.sql
+++ rest-spring-tomcat/src/main/resources/import.sql
@@ -1,3 +1,4 @@
 INSERT INTO PERSON (ID, FIRSTNAME, MIDDLENAME, LASTNAME, EMAIL, PHONE) VALUES (1, 'John', null, 'Doe', 'john.doe@sixturtle.com', '(512) 456-1001');
 INSERT INTO PERSON (ID, FIRSTNAME, MIDDLENAME, LASTNAME, EMAIL, PHONE) VALUES (2, 'Mary', null, 'Doe', 'mary.doe@sixturtle.com', '(512) 456-1002');
-ALTER SEQUENCE hibernate_sequence RESTART WITH 3;
+INSERT INTO PERSON (ID, FIRSTNAME, MIDDLENAME, LASTNAME, EMAIL, PHONE) VALUES (3, 'Jason', null, 'Doe', 'jason.doe@sixturtle.com', '(512) 456-1003');
+ALTER SEQUENCE hibernate_sequence RESTART WITH 4;
